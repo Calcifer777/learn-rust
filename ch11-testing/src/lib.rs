029c0b891eb2f253132592f93e71640b9252d4c3
Marco Filippone <m.filip@hotmail.it>
2023-10-28 23:26:45 +0200

    unit tests

---
+++ ch11-testing/src/lib.rs
@@ -0,0 +1,6 @@
+
+pub mod tests;
+
+fn adder(i: i32) -> i32 {
+    i + 2
+}
---
+++ ch11-testing/src/tests/test_adder.rs
@@ -0,0 +1,30 @@
+use super::super::*;
+
+#[test]
+fn test_adder() {
+    let out = adder(2);
+    assert!(out==5, "adder(2) = {} != 5", out);
+}
+
+#[test]
+fn test_with_assert_eq() {
+    let out = adder(2);
+    assert_eq!(out, 5);
+}
+
+#[test]
+fn test_handled_failure() -> Result<(), String> {
+    let exp = 42;
+    let out = adder(1);
+    if out == exp {
+        Ok(())
+    } else {
+        Err(format!("Expected {} but got {}", exp, out))
+    }
+}
+
+#[test]
+#[should_panic]
+fn test_expected_failure() {
+    panic!("Make this test fail");
+}
