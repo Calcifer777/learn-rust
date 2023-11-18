e53588970ed5cfe811cbdf935797006c529fa3b2
Marco Filippone <m.filip@hotmail.it>
2023-11-19 00:41:34 +0100

    isolate tests

--- zero2prod/src/configuration.rs
+++ zero2prod/src/configuration.rs
@@ -25,6 +25,16 @@ impl DatabaseSettings {
             self.name,
         )
     }
+
+    pub fn connection_string_without_db(&self) -> String {
+        format!(
+            "postgres://{}:{}@{}:{}",
+            self.username, 
+            self.password, 
+            self.host, 
+            self.port
+        )
+    }
 }
 
 pub fn get_configuration(fname: &str) -> Result<Settings, config::ConfigError> {
--- zero2prod/tests/subscription.rs
+++ zero2prod/tests/subscription.rs
@@ -1,32 +1,60 @@
-use actix_web::{test, App};
+use actix_web::{test, App, web};
 use sqlx::PgPool;
+use uuid::Uuid;
 use zero2prod::{routes::subscription::subscribe, configuration::get_configuration};
 
 
+async fn init_db() -> web::Data<PgPool> {
+    let mut cfg = get_configuration("./tests/resources/configuration.yaml")
+        .unwrap();
+    // Create temp db
+    let db_pool = PgPool::connect(
+        &cfg.database.connection_string()
+    )
+        .await
+        .unwrap();
+    let temp_db = Uuid::new_v4().to_string();
+    sqlx::query(
+        format!(r#"CREATE DATABASE "{}";"#, temp_db).as_str()
+    )
+        .execute(&db_pool)
+        .await
+        .expect("Failed to create database.");
+    // Setup tmp db
+    cfg.database.name = temp_db;
+    let db_pool = PgPool::connect(
+        &cfg.database.connection_string()
+        )
+        .await
+        .unwrap();
+    sqlx::migrate!("./../migrations")
+        .run(&db_pool)
+        .await
+        .expect("Failed to migrate the database");
+    web::Data::new(db_pool)
+}
+
 #[actix_web::test]
 async fn test_subscribe_ok() {
+    let db_pool = init_db().await;
     let app = test::init_service(
-        App::new().service(subscribe)
+        App::new()
+        .service(subscribe)
+        .app_data(db_pool.clone())
     ).await;
     let req = test::TestRequest::post()
         .uri("/subscription")
-        .set_json(&serde_json::json!({"name": "marco", "email": "test@gmail.com"}))
-        .to_request()
-        ;
+        .set_json(&serde_json::json!({"name": "marco", "email": "t@gmail.com"}))
+        .to_request();
     let rsp = test::call_service(&app, req).await;
     assert!(rsp.status().is_success());
     
     // Check db status
-    let cfg = get_configuration("./tests/resources/configuration.yaml")
-        .expect("Failed to read configuration.");
-    let conn_str = cfg.database.connection_string();
-    let pool = PgPool::connect(&conn_str)
+    let result = sqlx::query!("SELECT * FROM subscriptions")
+        .fetch_one(db_pool.get_ref())
         .await
-        .expect("DB connection failed");
-    let sub = sqlx::query!("SELECT * FROM subscriptions")
-        .fetch_one(&pool)
-        .await
-        .expect("Failed to fetch saved subscription")
-        ;
-    
+        .expect("Failed to fetch saved subscription");
+    assert_eq!(result.name, "marco");
+    assert_eq!(result.email, "t@gmail.com");
+
 }
